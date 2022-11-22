319388ddedf63305353466757deb98418f2b977b
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2022-11-22 16:20:46 +0100

    Create ExordeToken.sol

---
+++ contracts/token/ExordeToken.sol
@@ -0,0 +1,13 @@
+
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.8;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+
+contract ExordeToken is  ERC20Capped {
+    constructor() public ERC20 ("Exorde Network Token", "EXD") ERC20Capped(200*(10**6)) {
+        _mint(msg.sender, 200*(10**6)); // 200 000 000 (two hundred millions EXD)
+    }
+}
