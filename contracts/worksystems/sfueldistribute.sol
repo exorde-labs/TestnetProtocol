2ac901382609b7a8880b8e7adbe04bdaa86c1c83
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2022-11-30 15:33:33 +0100

    Update sfueldistribute.sol

--- contracts/worksystems/sfueldistribute.sol
+++ contracts/worksystems/sfueldistribute.sol
@@ -62,7 +62,7 @@ contract SFuelContracts is Ownable {
     event ReturnedSFuel(address indexed returner, address indexed whitelistedContract, uint256 amount);
 
     constructor() {
-        MIN_USER_BALANCE = 0.001 ether;
+        MIN_USER_BALANCE = 0.0001 ether;
         MIN_CONTRACT_BALANCE = 0.01 ether;
         isPaused = false;
     }
@@ -80,6 +80,11 @@ contract SFuelContracts is Ownable {
     modifier isActive() {
         require(!isPaused, "Contract is Paused");
         _;
+    }
+
+    function updateMinUserBalance(uint256 min_balance_) onlyOwner public{
+        require(min_balance_>0, "MIN_USER_BALANCE must be positive");
+        MIN_USER_BALANCE = min_balance_;
     }
 
     ///  Used by other contracts as a function to top up S-Fuel
