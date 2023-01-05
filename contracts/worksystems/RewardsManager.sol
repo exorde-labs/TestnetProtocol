8ae4a97a824c39773eaa7eb44171b2abd0d7eafe
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-01-05 17:31:48 +0100

    Update RewardsManager.sol

--- contracts/worksystems/RewardsManager.sol
+++ contracts/worksystems/RewardsManager.sol
@@ -28,7 +28,7 @@ contract RewardsManager is Ownable {
     event UserTransferred(bytes32 name);
 
     IERC20 public token;
-    mapping(address => uint256) public rewards; // maps user's address to voteToken balance
+    mapping(address => uint256) public rewards;
     uint256 public ManagerBalance = 0;
     uint256 TotalRewards = 0;
     IParametersManager public Parameters;
@@ -56,6 +56,10 @@ contract RewardsManager is Ownable {
         token = IERC20(EXD_token);
     }
 
+    /**
+     * @notice Updates the Parameters Manager Address
+     * @param addr The address of the new contract
+     */
     function updateParametersManager(address addr) public onlyOwner {
         require(addr != address(0));
         Parameters = IParametersManager(addr);
@@ -69,16 +73,28 @@ contract RewardsManager is Ownable {
     event RewardsWhitelisted(address indexed account, bool isWhitelisted);
     event RewardsUnWhitelisted(address indexed account, bool isWhitelisted);
 
+    /**
+     * @notice Returns if a contract address (or user) is whitelisted to interact with rewards
+     * @param _address The address
+     */
     function isRewardsWhitelisted(address _address) public view returns (bool) {
         return RewardsWhitelistMap[_address];
     }
 
+    /**
+     * @notice Adds an address as whitelisted to interact with rewards
+     * @param _address The address
+     */
     function addAddress(address _address) public onlyOwner {
         require(RewardsWhitelistMap[_address] != true, "RewardsManager: address must not be whitelisted already");
         RewardsWhitelistMap[_address] = true;
         emit RewardsWhitelisted(_address, true);
     }
 
+    /**
+     * @notice Removes an address as whitelisted to interact with rewards
+     * @param _address The address
+     */
     function removeAddress(address _address) public onlyOwner {
         require(RewardsWhitelistMap[_address] != false, "RewardsManager: address must be whitelisted to remove");
         RewardsWhitelistMap[_address] = false;
@@ -88,16 +104,18 @@ contract RewardsManager is Ownable {
     // ---------- EXTERNAL Rewards ALLOCATIONS ----------
 
     /**
-     * @notice A method for a verified whitelisted contract to allocate for itself some Rewards // nonReentrant()
+     * @notice A method for a verified whitelisted contract to allocate some Rewards
+     * @param _RewardsAllocation The rewards to allocate (distribute)
+     * @param _user The address of the user to credit _RewardsAllocation
      */
     function ProxyAddReward(uint256 _RewardsAllocation, address _user) external returns (bool) {
         require(isRewardsWhitelisted(msg.sender), "RewardsManager: sender must be whitelisted to Proxy act");
         // require(ManagerBalance >=  _RewardsAllocation);
         require(_RewardsAllocation > 0, "rewards to allocate must be positive..");
-        updateHourlyRewardsCount();
-        updateDailyRewardsCount();
+        // ---- Pre rewards distribution limitation check
         require(getDailyRewardsCount() < MAX_DAILY_EXD_REWARDS, "Daily Total Rewards exceed!");
         require(getHourlyRewardsCount() < MAX_HOURLY_EXD_REWARDS, "Hourly Total Rewards exceed!");
+        // ---- Pre rewards distribution limitation check
         // check if the contract calling this method has rights to allocate from user Rewards
         if (ManagerBalance >= _RewardsAllocation) {
             ManagerBalance -= _RewardsAllocation;
@@ -106,11 +124,20 @@ contract RewardsManager is Ownable {
             return true;
         }
         HourlyRewardsFlowManager[HourlyRewardsFlowManager.length - 1].counter +=  _RewardsAllocation;
+        DailyRewardsFlowManager[DailyRewardsFlowManager.length - 1].counter +=  _RewardsAllocation;
+        // ---- Post rewards distribution limitation check
+        updateHourlyRewardsCount();
+        updateDailyRewardsCount();
+        require(getDailyRewardsCount() < MAX_DAILY_EXD_REWARDS, "Daily Total Rewards exceed!");
+        require(getHourlyRewardsCount() < MAX_HOURLY_EXD_REWARDS, "Hourly Total Rewards exceed!");
+        // ---- Pre rewards distribution limitation check
         return (false);
     }
 
     /**
-     * @notice A method for a verified whitelisted contract to transferRewards // nonReentrant()
+     * @notice A method for a verified whitelisted contract to transfer rewards between two addresses
+     * @param _initial The address of the user to transfer from
+     * @param _receiving The address of the user to transfer to
      */
     function ProxyTransferRewards(address _initial, address _receiving) external returns (bool) {
         require(isRewardsWhitelisted(msg.sender), "RewardsManager: sender must be whitelisted to Proxy act");
@@ -124,12 +151,19 @@ contract RewardsManager is Ownable {
     }
 
 
+    /**
+     * @notice Returns the rewards balance (currently withdrawable and available) of a given user
+     * @param _address The address of the user
+     */
     function RewardsBalanceOf(address _address) public view returns (uint256) {
         return rewards[_address];
     }
 
     // ---------- ----------
 
+    /**
+     * @notice Updates the hourly sliding rewards counter
+     */
     function updateHourlyRewardsCount() public {
         uint256 last_timeframe_idx_ = HourlyRewardsFlowManager.length - 1;
         uint256 mostRecentTimestamp_ = HourlyRewardsFlowManager[last_timeframe_idx_].timestamp;
@@ -144,6 +178,9 @@ contract RewardsManager is Ownable {
         }
     }
 
+    /**
+     * @notice Updates the daily sliding rewards counter
+     */
     function updateDailyRewardsCount() public {
         uint256 last_timeframe_idx_ = DailyRewardsFlowManager.length - 1;
         uint256 mostRecentTimestamp_ = DailyRewardsFlowManager[last_timeframe_idx_].timestamp;
@@ -159,8 +196,8 @@ contract RewardsManager is Ownable {
     }
 
     /**
-  * @notice Count the total EXD rewards on last hour
-  */
+    * @notice Count the total EXD rewards on last hour
+    */
     function getHourlyRewardsCount() public view returns (uint256) {
         uint256 total = 0;
         for (uint256 i = 0; i < HourlyRewardsFlowManager.length; i++) {
@@ -170,8 +207,8 @@ contract RewardsManager is Ownable {
     }
 
     /**
-  * @notice Count the total EXD rewards on last day
-  */
+    * @notice Count the total EXD rewards on last day
+    */
     function getDailyRewardsCount() public view returns (uint256) {
         uint256 total = 0;
         for (uint256 i = 0; i < DailyRewardsFlowManager.length; i++) {
@@ -183,6 +220,11 @@ contract RewardsManager is Ownable {
 
     // ---------- DEPOSIT  MECHANISMS ----------
 
+    /**
+    * @notice Deposit _numTokens ERC20 EXD tokens to fill the RewardsManager balance
+    * This function will likely be called by the Exorde DAO and Exorde Labs
+    * @param _numTokens The number of ERC20 tokens to deposit
+    */
     function deposit(uint256 _numTokens) public {
         require(token.balanceOf(msg.sender) >= _numTokens, "RewardsManager: sender doesn't have enough tokens");
         // add the deposited tokens into existing balance
@@ -197,6 +239,10 @@ contract RewardsManager is Ownable {
 
     // ---------- WITHDRAWAL  MECHANISMS ----------
 
+    /**
+    * @notice Withdraw EXD rewards tokens associated to the user calling this function (the msg.sender)
+    * @param _numTokens The number of ERC20 tokens to withdraw
+    */
     function WithdrawRewards(uint256 _numTokens) external {
         require(
             ManagerBalance >= _numTokens,
@@ -206,6 +252,9 @@ contract RewardsManager is Ownable {
         require(token.transfer(msg.sender, _numTokens), "RewardsManager: WithdrawRewards- error transfering tokens");
     }
 
+    /**
+    * @notice Withdraw all rewards tokens associated to the user calling this function (the msg.sender)
+    */
     function WithdrawAllRewards() external {
         require(
             ManagerBalance >= rewards[msg.sender],
@@ -219,30 +268,11 @@ contract RewardsManager is Ownable {
         );
     }
 
-    function WithdrawSubworker(uint256 _numTokens, address _worker) external {
-        require(Parameters.getAddressManager() != address(0), "AddressManager is null in Parameters");
-        IAddressManager _AddressManager = IAddressManager(Parameters.getAddressManager());
-        require(ManagerBalance >= _numTokens, "ManagerBalance has to be >= _numTokens");
-        require(_AddressManager.isSubAddress(msg.sender, _worker)); //1st is supposed master, 2nd is sub address
-        rewards[_worker] -= _numTokens;
-        require(token.transfer(msg.sender, _numTokens), "Token Transfer failed");
-    }
-
-    function withdrawSubworkerAllRewards(address _worker) external {
-        require(Parameters.getAddressManager() != address(0), "AddressManager is null in Parameters");
-        IAddressManager _AddressManager = IAddressManager(Parameters.getAddressManager());
-        require(ManagerBalance >= rewards[_worker], "ManagerBalance has to be >= worker rewards");
-        require(_AddressManager.isSubAddress(msg.sender, _worker)); //1st is supposed master, 2nd is sub address
-        uint256 all_rewards = rewards[msg.sender];
-        rewards[msg.sender] = 0;
-        require(token.transfer(msg.sender, all_rewards), "Token Transfer failed");
-    }
-
-    // ---------- OWNER RARE MECHANISMS ----------
-
+    // ---------- OWNER MECHANISMS ----------
     /**
-    @notice Withdraw _numTokens ERC20 tokens from the voting contract, revoking these voting rights
-    @param _numTokens The number of ERC20 tokens desired in exchange for voting rights
+    * @notice Withdraw _numTokens ERC20 tokens from this rewards contracts
+    *         Only callable by the owner
+    * @param _numTokens The number of ERC20 tokens to withdraw
     */
     function OwnerWithdraw(uint256 _numTokens) external onlyOwner {
         require(ManagerBalance >= _numTokens, "ManagerBalance has to be >= _numTokens");
@@ -250,12 +280,16 @@ contract RewardsManager is Ownable {
         require(token.transfer(msg.sender, _numTokens), "Token Transfer failed");
     }
 
+    /**
+    * @notice returns the total amount of rewards distributed, during the life of this RewardsManager
+    */
     function GetTotalGivenRewards() public view returns (uint256) {
         return TotalRewards;
     }
 
     /**
-    @notice Withdraw _numTokens ERC20 tokens from the voting contract, revoking these voting rights
+    * @notice Withdraw all rewards in the remaining pool from the Rewards Manager, to the contract owner
+    * Usable in case of emergency or contract migration
     */
     function OwnerWithdrawAllRewards() external onlyOwner {
         require(ManagerBalance > 0, "ManagerBalance has to be > 0");
