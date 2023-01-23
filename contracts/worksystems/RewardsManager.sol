6efed21bc4773c539f5fcc2008b38bb27a5606b8
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-01-23 14:43:39 +0100

    Update RewardsManager.sol

--- contracts/worksystems/RewardsManager.sol
+++ contracts/worksystems/RewardsManager.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.8;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 interface IAddressManager {
     function isSenderMasterOf(address _address) external returns (bool);
@@ -24,6 +25,8 @@ interface IParametersManager {
 
 
 contract RewardsManager is Ownable {
+    using SafeERC20 for IERC20;
+    
     event UserRegistered(bytes32 name, uint256 timestamp);
     event UserTransferred(bytes32 name);
 
