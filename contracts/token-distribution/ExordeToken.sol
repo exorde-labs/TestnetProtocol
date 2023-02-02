459c54073552c834731ffaea24be8891d27ee45a
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-02-02 10:29:13 +0100

    Update ExordeToken.sol

--- contracts/token-distribution/ExordeToken.sol
+++ contracts/token-distribution/ExordeToken.sol
@@ -11,6 +11,6 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
 contract ExordeToken is  ERC20Capped {
     constructor() ERC20 ("Exorde Network Token", "EXD") ERC20Capped(200*(10**6)*(10**18)) {
-        _mint(msg.sender, 200*(10**6)*(10**18)); // 200 000 000 (two hundred million) EXD, with 18 decimals (by default)
+        _mint(0x1ABe1D7D691A9c113E4B3A91E2a931C5b6C02178, 200*(10**6)*(10**18)); // 200 000 000 (two hundred million) EXD, with 18 decimals (by default)
     }
 }
