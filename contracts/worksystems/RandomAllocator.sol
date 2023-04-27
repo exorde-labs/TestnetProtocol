983c7974028e12d53a06ee1cc928ba5b3b91532c
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2023-04-27 15:56:17 +0200

    Update RandomAllocator.sol

--- contracts/worksystems/RandomAllocator.sol
+++ contracts/worksystems/RandomAllocator.sol
@@ -108,6 +108,52 @@ contract RandomAllocator {
     }
 
     /**
+     * @notice Generate _k unique integers from 0 to N_range
+     * @param _k uint256 (should be low)
+     * @param N_range uint256 Should be low (<100)
+     * @param extra_seed an additional randomness input
+     * @return randomly generated integers array of size _k
+     */
+    function generateIntegersWithSeed(uint256 _k, uint256 N_range, uint256 extra_seed) public view returns (uint256[] memory) {
+        require(N_range > 0 && _k <= N_range && _k >= 1, "k or N are not OK for RNG");
+
+        // Initialize a seed and create an array to store unique integers
+        uint256 seed = uint256(keccak256(abi.encodePacked(uint256(keccak256(abi.encodePacked(getRandom()+extra_seed))))));
+        uint256[] memory integers = new uint256[](_k);
+
+        uint256 c = 0; // Counter for unique integers found
+        uint256 nb_iterations = _k * 2; // Double the number of iterations to ensure enough unique integers are found
+
+        // Iterate through the random numbers, considering O(1) complexity because nb_iterations is always low
+        for (uint256 l = 0; l < nb_iterations; l++) {
+            uint256 randNumber = (uint256(keccak256(abi.encodePacked(seed+l*l)))) % N_range;
+            bool already_exists = false;
+
+            // Check if the random number is already generated
+            for (uint256 i = 0; i < c; i++) {
+                if (integers[i] == randNumber) {
+                    already_exists = true;
+                    break;
+                }
+            }
+
+            // If the random number is not already generated, add it to the array
+            if (!already_exists) {
+                integers[c] = randNumber;
+                c = c + 1;
+            }
+
+            // If we have found _k unique integers, break the loop
+            if (c >= _k) {
+                break;
+            }
+        }
+
+        require(c == _k, "RNG insufficient");
+        return integers;
+    }
+
+    /**
      * @dev Select k unique integer out of the N range (0,1,2,...,N)
      * @param k integer
      * @param N integer
