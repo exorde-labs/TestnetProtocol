d8216bb000e16286554e3f49cacc8a8bb70849a6
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2024-01-18 16:15:31 +0100

    Update RandomSubsets.sol

--- contracts/worksystems/worksystems_v2_2023/RandomSubsets.sol
+++ contracts/worksystems/worksystems_v2_2023/RandomSubsets.sol
@@ -25,17 +25,17 @@ contract RandomSubsets is RandomAllocator {
     *         and contains two elements: the start index and the end index (inclusive) of the subset. If the function fails to generate `K`
     *         subsets after `maxFailedAttempts`, it returns an empty array.
     */
-    function _randomSubsets(uint128 K, uint128 N, uint128 L, uint128 M, uint128 maxFailedAttempts) private view returns (uint256[][] memory) {
+    function _randomSubsets(uint128 K, uint128 N, uint128 L, uint128 M, uint128 maxFailedAttempts) private view returns (uint128[][] memory) {
         require(K > 0 && L > 0 && M > 0 && M <= L && K * L <= N, "Input is not valid for generating non-overlapping subsets");
 
-        uint256[][] memory subsets = new uint256[][](K);
+        uint128[][] memory subsets = new uint128[][](K);
 
         uint128 highestIndex = 0;
         uint128 failedAttempts = 0;
         uint128 i = 0;
 
         // Keep track of the generated base indices using an array
-        uint256[] memory generatedIndices = new uint256[](K);
+        uint128[] memory generatedIndices = new uint128[](K);
 
         while (i < K) {
             // Calculate available space and space needed for the remaining subsets
@@ -74,7 +74,7 @@ contract RandomSubsets is RandomAllocator {
                 if (!indexExists) {                                
                     // Generate random subsets of length between M and L
                     // Create the subset using the generated base index and subset length
-                    subsets[i] = new uint256[](2);
+                    subsets[i] = new uint128[](2);
                     subsets[i][0] = baseIndex;
                     subsets[i][1] = newSubsetEnd;
 
@@ -118,7 +118,7 @@ contract RandomSubsets is RandomAllocator {
 
             If the input is not valid for generating non-overlapping subsets, the function will revert.
     */
-    function getStandardSubsets(uint128 K_, uint128 N_, uint128 coverage) public view returns (uint256[][] memory) {
+    function getRandomSubsets(uint128 K_, uint128 N_, uint128 coverage) public view returns (uint128[][] memory) {
         require(coverage > 0 && coverage <= 100, "coverage must be between 1 and 100");
         // Calculate the minimum and maximum subset lengths and the maximum failed attempts based on the input parameters.
         (uint128 M_, uint128 L_, uint128 maxFailedAttempts_) = _getParameters(K_, N_, coverage);
@@ -131,7 +131,7 @@ contract RandomSubsets is RandomAllocator {
         The function uses the formula M_ = (N_ * coverage) / (K_ * 100 * 4) to calculate the lower bound of the subset length and sets the upper bound to L_ = 4 * M_. It then computes the maximum failed attempts using the formula:
     
         maxFailedAttempts_ >= (K_ * N_ - K_ * L_ - N_ + L_ + ln[(N_ - L_ + 1 choose K_)] - ln(1 - 0.99)) / ln(1 - L_ / N_)
-        We approximate this to 15 in general.
+        We approximate this to 10.
         The value 0.99 represents the probability of generating a single non-overlapping subset and the formula ensures that all K_ subsets are generated with at least 99% probability. The function returns the calculated values as a tuple.
         @param K_ The number of required non-overlapping subsets.
         @param N_ The range of integers from which to generate the subsets.
@@ -140,13 +140,13 @@ contract RandomSubsets is RandomAllocator {
         @return L_ The upper bound of the subset length.
         @return maxFailedAttempts_ The maximum number of failed attempts that should be allowed during the subset generation process.
     */
-    function _getParameters(uint128 K_, uint128 N_, uint128 coverage) private pure returns (uint128, uint128, uint128) {
+    function _getParameters(uint128 K_, uint128 N_, uint128 coverage) public pure returns (uint128, uint128, uint128) {
         require(N_ >= 50, "N must be >= 50");
         require(coverage > 0 && coverage <= 100, "P must be between 1 and 100");
         // Calculate the minimum and maximum subset lengths and the maximum failed attempts based on the input parameters.
         uint128 M_ = uint128((N_ * coverage) / (K_ * 100.0 * 2)); // Subset length lower bound
         uint128 L_ = M_ * 4; // Subset length upper bound
-        uint128 maxFailedAttempts_ = 15; // Maximum failed attempts
+        uint128 maxFailedAttempts_ = 10; // Maximum failed attempts
         // Generate the random non-overlapping subsets using the calculated minimum and maximum subset lengths and maximum failed attempts.
         if ( M_ == 0 || L_ == 0 ){
             M_ = N_ / 10 + 1;
@@ -162,15 +162,15 @@ contract RandomSubsets is RandomAllocator {
         @param coverage The coverage array containing the starting and ending indexes of each subset.
         @return A tuple containing the number of non-empty subsets, the length of each subset, the total length of all non-empty subsets, and the coverage percentage of the superset.
     */
-    function getCoverage(uint128 K_, uint128 N_, uint128 coverage) public view returns (uint256, uint256[] memory, uint256, uint256) {
-        uint256[][] memory subsets = getStandardSubsets(K_, N_, coverage);
-        uint256[] memory subset_lengths = new uint256[](subsets.length);
-        uint256 cumulative_length;
-        uint256 nb_subsets = 0;
-        uint256 coverage_percentage = 0;
+    function getCoverage(uint128 K_, uint128 N_, uint128 coverage) public view returns (uint128, uint128[] memory, uint128, uint128) {
+        uint128[][] memory subsets = getRandomSubsets(K_, N_, coverage);
+        uint128[] memory subset_lengths = new uint128[](subsets.length);
+        uint128 cumulative_length;
+        uint128 nb_subsets = 0;
+        uint128 coverage_percentage = 0;
         for (uint i = 0; i < subsets.length; i++) {
             if (subsets[i].length > 0) { // Check if subset is not empty
-                uint256 length = subsets[i][1] - subsets[i][0] + 1;
+                uint128 length = subsets[i][1] - subsets[i][0] + 1;
                 subset_lengths[nb_subsets] = length;
                 nb_subsets++;
                 cumulative_length += length;
